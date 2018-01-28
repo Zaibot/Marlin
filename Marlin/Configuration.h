1d8e1c7016e8f80f0a2f274b24ea1933f04f5a59
Tobias de Groen <tdgroen@zaibot.net>
2018-01-28 18:47:00 +0100

    Extruder gear (92.5925)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -532,7 +532,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 95 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 93.7745 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 92.5925 }
 
 /**
  * Default Max Feed Rate (mm/s)
